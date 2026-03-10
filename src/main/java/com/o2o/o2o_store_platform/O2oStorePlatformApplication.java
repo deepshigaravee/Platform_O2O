097874592b2bb540ba022012f284a590e5852f9f
Charvitha Reddy <2472006@cognizant.com>
2026-03-10 09:13:43 +0530

    Initial commit

---
+++ src/main/java/com/o2o/o2o_store_platform/O2oStorePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.o2o.o2o_store_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O2oStorePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O2oStorePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/o2o/o2o_store_platform/O2oStorePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o2o.o2o_store_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O2oStorePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
